a9b46c03f9516e135a6a9c2d090b376112c66189
girish <agraharigirish@gmail.com>
2024-04-04 17:59:48 +0530

    first_comment

---
+++ src/main/java/com/test/docker/DockerApplication.java
@@ -0,0 +1,23 @@
+package com.test.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/docker/v1")
+public class DockerApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Testing Docker.....";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
